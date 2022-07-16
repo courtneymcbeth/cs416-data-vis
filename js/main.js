796fdc3cb37d7d776371c6e8895cb61caad4e88d
Courtney McBeth <clm325@cornell.edu>
2022-07-16 17:53:25 -0500

    Adding geojson file

--- js/main.js
+++ js/main.js
@@ -4,6 +4,16 @@ var height = 500;
 var projection = d3.geo.albersUsa().translate([width/2, height/2]).scale([1000]);
 var path = d3.geo.path().projection(projection);
 
+d3.json("https://courtneymcbeth.github.io/cs416-data-vis/data/states.json", function(json) {
+  d3.select('#locations').select('svg').selectAll("path")
+	.data(json.features)
+	.enter()
+	.append("path")
+	.attr("d", path)
+	.style("stroke", "#000")
+	.style("stroke-width", "1")
+  .style("fill", "#fff")
+
 d3.csv("https://courtneymcbeth.github.io/cs416-data-vis/data/mod_MERGED2020_21_PP.csv", function(data) {
   d3.select('#locations').select('svg').selectAll("circle")
 	.data(data)
@@ -27,4 +37,5 @@ d3.csv("https://courtneymcbeth.github.io/cs416-data-vis/data/mod_MERGED2020_21_P
     }
 		return Math.sqrt(d.UG + d.GRADS) / 10;
 	});
+});
 });
